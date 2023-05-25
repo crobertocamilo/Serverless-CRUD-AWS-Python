2ce4940ac45a8ff09d19f39d4a1ac2f12e8b1659
Carlos Roberto S Camilo <carlos.rs.camilo@unesp.br>
2023-05-25 17:48:03 -0300

    feat: criada funcao Lambda para recuperar/listar os itens da tabela

---
+++ src/fetchItems.py
@@ -0,0 +1,50 @@
+import boto3
+import json
+import logging 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+import decimal
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('Cities')
+
+def handler(event, contest):
+
+    logger.info(event)
+
+    try:
+
+        response = table.scan()
+        result = response['Items']
+
+        while 'LastEvaluatedKey' in response:
+            response = table.scan(ExclusiveStartKey = response['LastEvaluatedKey'])
+            result.extend(response['Items'])
+
+        body = {
+            'Cidade': result
+
+        }
+
+        return buildResponse(200, body)
+    
+    except:
+        logger.exception('Erro ao realizar a pesquisa!')
+
+def buildResponse(statusCode, body=None):
+    response = {
+        'statusCode': statusCode,
+        'headers': {
+            'ContentType': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        }
+    }
+    if body is not None:
+        response['body'] = json.dumps(body, default=decimal_default)
+    
+    return response
+
+def decimal_default(obj):
+    if isinstance(obj, decimal.Decimal):
+        return float(obj)
+    raise TypeError
